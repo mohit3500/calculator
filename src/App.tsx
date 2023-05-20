b415e0962a245ab3597c363af5a34dfee2cd7f52
Mohit Yadav <yadavmohit3147@gmail.com>
2023-05-20 23:36:01 +0530

    second-commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
 import { useState } from 'react';
+import PageNotFound from './PageNotFound';
 
 function App() {
   const [name, setName] = useState<string>('');
@@ -11,10 +12,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CalculatorHome name = {name} setName = {setName}/>} />
+        <Route
+          path="/"
+          element={<CalculatorHome name={name} setName={setName} />}
+        />
         <Route path="/calculator" element={<Calculator name={name} />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
